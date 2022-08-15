afd7e0086f4f1e32a87fed1ceca5c2834119b88a
nate <wardnw@gmail.com>
2022-08-14 23:25:15 -0700

    removed commented lines

--- app.js
+++ app.js
@@ -5,18 +5,15 @@ function nextValue(){
 
 	if (value % 3 === 0 && value % 5 > 0){
 		console.log("fizz")
-		//value++
 	}
 	else if (value % 5 === 0 && value % 3 > 0){
 		console.log("buzz")
 	}
 	else if (value % 5 === 0 && value % 3 === 0){
 		console.log("fizzbuzz")
-		//value++
 	}
 	else{
 		console.log(value)
-		//value++
 	}
 	value++ 
 }
