3587924e1aea20f866762bc12c4e7e8ffec35504
Nate Ward <wardnw@gmail.com>
2022-08-15 09:23:49 -0700

    default to 1, minor logic changes

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let numberDisplay = document.getElementById("numberDisplay")
-let value = 0 
+let value = 1 
 
 function nextValue(){
 
-	if (value % 3 === 0 && value % 5 > 0){
+	if (value % 3 === 0 && value % 5 === 0){
+		console.log("fizzbuzz")
+	}
+	else if (value % 3 === 0 && value % 5 > 0){
 		console.log("fizz")
 	}
 	else if (value % 5 === 0 && value % 3 > 0){
 		console.log("buzz")
-	}
-	else if (value % 5 === 0 && value % 3 === 0){
-		console.log("fizzbuzz")
 	}
 	else{
 		console.log(value)
